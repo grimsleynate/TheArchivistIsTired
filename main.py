1c4ede1ba05e684dc7a33e7c6db90f539e9e02d2
Nathaniel Grimsley <grimsleynate@gmail.com>
2026-07-07 19:36:33 -0400

    Refactored for cleanliness; added hp and damage to Actors

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import entity_factories
 
 from tcod import libtcodpy # type: ignore
 from engine import Engine
-from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -24,22 +23,20 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-
-    event_handler = EventHandler()
-
+    
     player = copy.deepcopy(entity_factories.player)
-
-    game_map = generate_dungeon(
+    engine = Engine(player=player)
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_max_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        engine=engine
     )
-
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    
+    engine.update_fov()
 
     with tcod.context.new(
         columns=screen_width,
@@ -52,8 +49,7 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 
 if __name__ == "__main__":
